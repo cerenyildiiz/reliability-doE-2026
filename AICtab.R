6ba815130847716506ca77f8c8d531e9df39dff5
Ceren Yildiz <cerennyildiz634@gmail.com>
2026-07-10 00:02:20 +0300

    Update AICtab.R

--- AICtab.R
+++ AICtab.R
@@ -1,1 +1,36 @@
+ # install.packages(c("survival", "AICcmodavg"))
+library(survival)
+library(AICcmodavg)
 
+# Modeller
+model_weibull <- survreg(
+  Surv(Lifetime, Cens) ~ A + B + C + D + E + F + G,
+  data = data,
+  dist = "weibull"
+)
+
+model_lognormal <- survreg(
+  Surv(Lifetime, Cens) ~ A + B + C + D + E + F + G,
+  data = data,
+  dist = "lognormal"
+)
+
+model_exp <- survreg(
+  Surv(Lifetime, Cens) ~ A + B + C + D + E + F + G,
+  data = data,
+  dist = "exponential"
+)
+
+
+cand.models <- list(
+  Exponential = model_exp,
+  Weibull     = model_weibull,
+  Lognormal   = model_lognormal
+)
+
+# AICc tablosu
+aictab(
+  cand.set = cand.models,
+  modnames = names(cand.models),
+  second.ord = TRUE
+)
